c274f1155ffafe06435c0758580adca1d4cf7419
ZengJing <3051850092@qq.com>
2020-08-05 00:04:54 +0000

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,5 @@ Route::post('user_addresses', 'UserAddressesController@store')->name('user_addre
 Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
 Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
 Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
