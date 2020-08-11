a0c94f9c650b72353de3d3e89ce5d261d29d08d6
ZengJing <3051850092@qq.com>
2020-08-11 09:53:13 +0000

    管理后台商品控制器封装

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -15,6 +15,10 @@ use App\Models\Category;
 class ProductsController extends Controller
 {
     use HasResourceActions;
+    public function getProductType()
+    {
+        return Product::TYPE_CROWDFUNDING;
+    }
 
     /**
      * Index interface.
@@ -179,4 +183,26 @@ class ProductsController extends Controller
             });
         });
     }
+
+
+    protected function customGrid(Grid $grid)
+    {
+        $grid->model()->with(['category']);
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->column('category.name', '类目');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->rating('评分');
+        $grid->sold_count('销量');
+        $grid->review_count('评论数');
+    }
+
+    protected function customForm(Form $form)
+    {
+        // 普通商品没有额外的字段，因此这里不需要写任何代码
+    }
+
 }
